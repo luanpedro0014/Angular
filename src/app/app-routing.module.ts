db9316156d00de091081ef5b1e3f25b0bc90910a
luanpedro0014 <luanpedro0014@gmail.com>
2021-06-11 11:19:04 -0300

    Codigos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+
+{ path: '',redirectTo:'dashboard', pathMatch: 'full'},   //Quando não for digitado nenhuma informação pagina será redirecionado para a principaal(Dashboard)
+{path: 'dashboard',component:DashboardComponent},
+{path: 'alunos',component:AlunosComponent},
+{path: 'perfil',component:PerfilComponent},
+{ path: 'professores',component:ProfessoresComponent},
+
+
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
